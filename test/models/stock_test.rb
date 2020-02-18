3c514b0bc2e11224b00dc320812f5d2c161290a1
daisuke-hara0515 <wildlife402@gmail.com>
2020-02-18 09:29:33 +0900

    stockインスタンスが有効かどうかのテスト作成。 gemファイル一部修正

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @stock = Stock.new(name: "Example", stock: 1, unit: "g")
+  end
+
+  test "should be valid" do
+    assert @stock.valid?
+  end
 end

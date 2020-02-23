9dd14d8047676e7ee50418fe9fb640091cfa2ad5
daisuke-hara0515 <wildlife402@gmail.com>
2020-02-23 15:10:54 +0900

    デフォルトのテンプレートエンジンをslimに変更

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+class Application < Rails::Application
+  config.generators.template_engine = :slim
+end
+
 module Reizoko
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

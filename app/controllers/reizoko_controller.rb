fa80af2b16c1d269d33c90b3e9afb652feefbcd9
daisuke-hara0515 <wildlife402@gmail.com>
2020-03-16 22:33:23 +0900

    loginページとzaiko(在庫)ページ作っちゃったけど、とりあえず放置

--- app/controllers/reizoko_controller.rb
+++ app/controllers/reizoko_controller.rb
@@ -1,6 +1,13 @@
 class ReizokoController < ApplicationController
+    def login
+    end
+
+    def zaiko
+    end
+
     def new
     end
+    
     def form
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # get 'reizoko/index'
   root 'reizoko#index'
   resources :reizoko
+  get 'reizoko/login'
 end

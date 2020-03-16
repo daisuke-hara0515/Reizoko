97c273e91876e25bddf864d1cd7192053fe15047
daisuke-hara0515 <wildlife402@gmail.com>
2020-03-16 22:55:13 +0900

    controllerのzaikoアクションとnewアクションにコード追加

--- app/controllers/reizoko_controller.rb
+++ app/controllers/reizoko_controller.rb
@@ -3,9 +3,11 @@ class ReizokoController < ApplicationController
     end
 
     def zaiko
+        @stock = Stock.find(params[:id])
     end
 
     def new
+        @stock = Stock.new
     end
     
     def form

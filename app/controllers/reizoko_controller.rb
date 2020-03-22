7a566de311aa1e5c01b948fb76282de54d419c4d
daisuke-hara0515 <wildlife402@gmail.com>
2020-03-22 14:24:37 +0900

    reizoko_controllerにcreateアクションを実装

--- app/controllers/reizoko_controller.rb
+++ app/controllers/reizoko_controller.rb
@@ -11,4 +11,16 @@ class ReizokoController < ApplicationController
     
     def form
     end
+
+    def create
+        stock = Stock.new(stock_params)
+        stock.save!
+        redirect_to reizoko_url, notice: "「#{stock.name}」の登録が完了しました"
+    end
+
+    private
+    
+    def stock_params
+        params.require(:stock).permit(:name,:stock,:unit,:memo)
+    end
 end

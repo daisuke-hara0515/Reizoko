1b38dbce13a752c97cd0f871834b56862dee064d
daisuke-hara0515 <wildlife402@gmail.com>
2020-03-24 09:10:01 +0900

    createアクションとstrongparameterを実装

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,8 +4,17 @@ class StocksController < ApplicationController
     end
 
     def create
+        @stock = Stock.new(stock_params)
+        @stock.save!
+        redirect_to stocks
     end
 
     def show
+    end
+
+    private
+
+    def stock_params
+        params.require(:stock).permit(:name,:unit,:stock,:memo)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :stocks
+  root 'stocks#index'
 end

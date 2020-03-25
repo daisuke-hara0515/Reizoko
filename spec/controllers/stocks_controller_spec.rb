533a40501a7cd7409196f697dfd1bd8d159ba6df
daisuke-hara0515 <wildlife402@gmail.com>
2020-03-25 21:57:50 +0900

    Rspecで実行しないといけなさそうなテストを日本語でまずは書いてみた

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -4,10 +4,22 @@ describe StocksController do
     it 'indexページをgetを使って取得できる' do
         get :index
         assert_response :success
-        assert_select "title" , "Reizoko"
     end
     it 'newページをgetを使って取得できる' do
         get :new
         assert_response :success
     end
+
+    it 'rootページから新規登録リンクを踏んでnewページに移ること' do
+
+    end
+    
+    it '在庫名、個数、単位は空では登録できず、エラーが発生すること' do
+
+    end
+    
+    it '在庫名はstring、個数はinteger、単位はstring以外を入力したらエラーになること' do
+
+    end
+
 end

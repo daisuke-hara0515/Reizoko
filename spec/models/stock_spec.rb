f2407161787f76db2181e6fafbe546d7e744c1d9
daisuke-hara0515 <wildlife402@gmail.com>
2020-04-19 17:45:18 +0900

    テストコードを修正

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,20 +1,16 @@
 require "rails_helper"
 
-RSpec.describe 'Stockモデル',type: :model do
-    describe 'stockモデルの有効性'
-    before do
-        @stock = FactoryBot.create(:stock)
-    end
-
+RSpec.describe Stock,type: :model do
+  describe 'stockモデルの有効性' do
+    let(:stock) {Stock.new(name:'tomato',stock:1,unit:'個')}
     it 'stock.valid?' do
-        expect(@stock.valid?).to be true
+        expect(stock.valid?).to be true
     end
+  end
 
-    # it '登録している在庫情報をアップデートできること' do
-    #     # attributes_forは対象の属性をHashで返す、オプションで
-    #     stock_params = FactoryBot.attributes_for(:stock, name: "tomato")
-    #     patch stocks_path(stock), params: {id :stock.id, stock: stock_params}
-    #     expect(stock.reload.name).to eq "tomato"
-    # end
-
+    it '登録している在庫情報をアップデートできること' do
+        # 登録データをpatchしてupdateする
+        patch stock_url(tomato), stock: {name: "potato"}
+        expect(stock.reload.name).to eq "potato"
+    end
 end

f57f96020c88c76f94ee4a1108b50b7361d58af8
daisuke-hara0515 <wildlife402@gmail.com>
2020-05-25 09:13:31 +0900

    users_controller追記

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,22 @@
 class Admin::UsersController < ApplicationController
-  def new
+  def new #newメソッドで空っぽのインスタンスを作る
+    @user = User.new
+  end
+
+  def create #登録画面に入力されたparamsを引数にインスタンス変数作成
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+
+    private
+
+    def user_params #strong parameter
+      params.require(:user).permit(:name,:email,:admin,:password,:password_confirmation)
+    end
   end
 
   def edit

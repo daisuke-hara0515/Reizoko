2d24b7e2cc7d24106824d4df5bd79c741136f358
daisuke-hara0515 <wildlife402@gmail.com>
2020-05-31 12:11:22 +0900

    current_userを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # helper_methodはcontrollerに定義したメソッドをviewでも使えるようにする。
+    helper_method :current_user
+
+    private # クラスの外からアクセスできると情報が見えてしまうからprivateに隠す？
+    
+    def current_user
+      # @current_user = @current_user || User.find~で、左項から判定していきtrueになったら処理終了。
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end

8c4c27320cf606a11f017357057f280d13f5f732
daisuke-hara0515 <wildlife402@gmail.com>
2020-06-10 21:15:07 +0900

    ログインしたら在庫一覧に飛ぶように修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'ログイン成功'
+      redirect_to stocks_path, notice: 'ログイン成功'
     else
       render :new
     end

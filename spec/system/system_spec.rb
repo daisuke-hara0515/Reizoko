67998619985f946e0768e931b0b5ce41db989c1b
daisuke-hara0515 <wildlife402@gmail.com>
2020-06-17 13:39:04 +0900

    beforeとcontextを追加

--- spec/system/system_spec.rb
+++ spec/system/system_spec.rb
@@ -1,34 +1,25 @@
 require "rails_helper"
 
-RSpec.describe 'stockモデル',type: :system do
-  describe 'stock登録' do
-    it '登録ユーザーでログインすると在庫のindexが表示されること' do
-      # ログインページを取得する
-      # 登録ユーザーでログインする
-      # ログインが成功する(ログイン成功のnoticeが表示されること)
+RSpec.describe 'stock管理画面一覧',type: :system do
+  describe 'stock表示機能' do
+    before do
+      # テストユーザーを作成する
+      # テストユーザーのstockを作成する
+    end
+    context '登録ユーザーがログインしている時'
+      before do
+        # テストユーザーでログインする
+      end
+      
+      it '登録ユーザーが作成した在庫が表示される' do
       # indexアクションでstockモデルが取得できること
     end
 
-    it '登録ユーザーの在庫情報が更新できること'
-      # ログインページを取得する
-      # 登録ユーザーでログインする
-      # ログインが成功する
+    it '登録ユーザーの在庫が更新できること' do
       # 適当なstockの編集ボタンをクリックする
-      # 情報を書き換える
+      # 情報を入力する
       # 保存ボタンをクリックする
       # 更新が成功する(成功時のnoticeが表示される)
-    
-    
-
-
-  # 在庫情報の更新テストは後ほど作る
-  describe '在庫情報がアップデートできること' do
-    it '登録している在庫情報をアップデートできること' do
-        # 登録データをpatchしてupdateする
-        # このサイトでFactoryBotの基本載っている。https://www.yako-geppo.com/entry/rails-rspec-factorybot
-        # visit edit_stock_path(stock)
-        # put stock_path(stock),params: { stock: {name: "potato"}}
-        # expect(stock.reload).to be true
     end
   end
 end

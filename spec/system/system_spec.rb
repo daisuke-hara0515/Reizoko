4cbf56b4eabe55fb9a9aa0ac42cd721ea3deb922
daisuke-hara0515 <wildlife402@gmail.com>
2020-06-21 17:59:36 +0900

    letを使って、処理の共通化を図った

--- spec/system/system_spec.rb
+++ spec/system/system_spec.rb
@@ -2,34 +2,26 @@ require "rails_helper"
 
 RSpec.describe 'stock管理画面一覧',type: :system do
   describe 'stock表示機能' do
+    let(:user_a) {FactoryBot.create(:user, name: 'ユーザーA', email:'a@example.com')}
+    let(:user_b) {FactoryBot.create(:user, name: 'ユーザーB', email:'b@example.com')}
     before do
       # FactoryBotをわざわざ見に行かなくても良いように一部の属性を変更している
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email:'a@example.com')
+      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email:'a@example.com')  
       FactoryBot.create(:stock, name:'キャベツ', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
     context 'ユーザーAがログインしている時'
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
-
+      let(:login_user) {user_a}
       it 'ユーザーAが作成した在庫が表示される' do
         # indexアクションでstockモデルが取得できること
         expect(page).to have_content 'キャベツ'
       end
       
     context 'ユーザーBがログインしている時' do
-      before do
-        # ユーザーBを作成する
-        user_b = FactoryBot.create(:user, name: 'ユーザーB', email:'b@example.com')
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) {user_b}
 
       it 'ユーザーAが作成した在庫情報が表示されない' do
         expect(page).to have_no_content 'キャベツ'

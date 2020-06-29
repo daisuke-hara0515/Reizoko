df4476404523a03cf14c9c9646b4235f1ee0bd2c
daisuke-hara0515 <wildlife402@gmail.com>
2020-06-29 21:47:55 +0900

    UTCではなく、Asia/Tokyoの時刻を取得するようにした

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Reizoko
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     config.generators do |g|
       g.test_framework :rspec,

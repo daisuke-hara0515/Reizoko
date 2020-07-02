812c44332e3c358c8163ceee3c8196b02b70b3cf
daisuke-hara0515 <wildlife402@gmail.com>
2020-07-03 08:38:58 +0900

    マイグレーション実行

--- db/migrate/20200702144754_addexpire_date_to_stocks.rb
+++ db/migrate/20200702144754_addexpire_date_to_stocks.rb
@@ -1,4 +1,5 @@
 class AddexpireDateToStocks < ActiveRecord::Migration[5.2]
   def change
+    add_column :stocks, :expire_date, :timewithzone
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_143610) do
+ActiveRecord::Schema.define(version: 2020_07_02_144754) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_143610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.time "expire_date"
     t.index ["user_id"], name: "index_stocks_on_user_id"
   end
 

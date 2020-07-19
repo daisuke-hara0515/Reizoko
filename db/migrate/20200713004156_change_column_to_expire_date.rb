984507faa18906184c5c3ea08e5304e93d866a42
daisuke-hara0515 <wildlife402@gmail.com>
2020-07-19 10:20:02 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20200713004156_change_column_to_expire_date.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToExpireDate < ActiveRecord::Migration[5.2]
-  def up
-    change_column :stocks, :expire_date, :datetime, null: false
-  end
-
-  def down
-    change_column :stocks, :expire_date, :timewithzone
-  end
-end

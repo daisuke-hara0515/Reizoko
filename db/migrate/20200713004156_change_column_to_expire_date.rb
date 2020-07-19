3ef886011e24acf971ef553ceac912f922808f0e
daisuke-hara0515 <wildlife402@gmail.com>
2020-07-19 10:17:36 +0900

    ロールバック実行

--- db/migrate/20200713004156_change_column_to_expire_date.rb
+++ db/migrate/20200713004156_change_column_to_expire_date.rb
@@ -1,5 +1,9 @@
 class ChangeColumnToExpireDate < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :stocks, :expire_date, :datetime, null: false
+  end
+
+  def down
+    change_column :stocks, :expire_date, :timewithzone
   end
 end

a0fbc8048563a754d9ef3fc2c674e323ec104430
daisuke-hara0515 <wildlife402@gmail.com>
2020-08-25 23:36:07 +0900

    在庫番号を追加するコードを追記

--- db/migrate/20200825142928_add_controll_number_to_stocks.rb
+++ db/migrate/20200825142928_add_controll_number_to_stocks.rb
@@ -1,4 +1,10 @@
 class AddControllNumberToStocks < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    add_column :stocks, :control_number, :integer
   end
+
+  def down
+    remove_column :stocks, :control_number, :integer
+  end
+
 end

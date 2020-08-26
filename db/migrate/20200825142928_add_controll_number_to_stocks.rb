ba7e923f31d5b51098211aae161228b9d9f65334
daisuke-hara0515 <wildlife402@gmail.com>
2020-08-26 23:56:53 +0900

    色々迷ったが、管理番号はDBに保存するときはinteger型で保存することにする

--- db/migrate/20200825142928_add_controll_number_to_stocks.rb
+++ db/migrate/20200825142928_add_controll_number_to_stocks.rb
@@ -1,10 +1,10 @@
 class AddControllNumberToStocks < ActiveRecord::Migration[5.2]
   def up
-    add_column :stocks, :control_number, :string
+    add_column :stocks, :control_number, :integer
   end
 
   def down
-    remove_column :stocks, :control_number, :string
+    remove_column :stocks, :control_number, :integer
   end
 
 end

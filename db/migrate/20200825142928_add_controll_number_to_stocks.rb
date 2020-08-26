ff417afaa7b9ca93a656194e37217a26b9ca5016
daisuke-hara0515 <wildlife402@gmail.com>
2020-08-26 23:47:15 +0900

    integer→stringに変更

--- db/migrate/20200825142928_add_controll_number_to_stocks.rb
+++ db/migrate/20200825142928_add_controll_number_to_stocks.rb
@@ -1,10 +1,10 @@
 class AddControllNumberToStocks < ActiveRecord::Migration[5.2]
   def up
-    add_column :stocks, :control_number, :integer
+    add_column :stocks, :control_number, :string
   end
 
   def down
-    remove_column :stocks, :control_number, :integer
+    remove_column :stocks, :control_number, :string
   end
 
 end

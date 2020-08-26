3af266fcfa0db4711493426aaeecfb630d5e98f3
daisuke-hara0515 <wildlife402@gmail.com>
2020-08-26 23:45:49 +0900

    管理番号の属性をintegerからstringにしたいのでロールバック実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_142928) do
+ActiveRecord::Schema.define(version: 2020_07_25_073303) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_08_25_142928) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.datetime "expire_date", null: false
-    t.integer "control_number"
     t.index ["user_id"], name: "index_stocks_on_user_id"
   end
 
